f2b0224d990005fc68e38c1f93073311b88032a8
Tayler How <taylerhow@gmail.com>
2015-05-05 15:04:28 -0400

    Added memorization to MusicPlayer

--- src/state/StateManager.java
+++ src/state/StateManager.java
@@ -72,6 +72,9 @@ public class StateManager {
 	 * @param state the specified state to update.
 	 */
 	public void loadState(int state) {
+		//INSTRUMENTATION FOR CSSE376 LAB
+		long start = System.nanoTime();
+		
 		// Constants.
 		final int MENU_STATE = 0;
 		final int LEVEL_0 = 1;
@@ -108,6 +111,10 @@ public class StateManager {
 			this.states[state] = new LevelState(this, LEVEL_0 - 1);
 			this.isPlayingSurvival = true;
 		}
+		
+		//INSTRUMENTATION FOR CSSE376 LAB
+		long delta = System.nanoTime() - start;
+		System.err.println("Load time (ms): "+(delta/1000000L));
 	}
 	
 	/**
--- src/toolObjects/MusicPlayer.java
+++ src/toolObjects/MusicPlayer.java
@@ -16,12 +16,22 @@ public class MusicPlayer {
 	
 	private Clip clip;
 	
+	//INSTRUMENTATION FOR CSSE376 LAB
+	static HashMap<String, Clip> clips=new HashMap<String, Clip>();
+	
 	/**
 	 * Constructs a Music Player.
 	 *
 	 * @param fileName the destination of the media file.
 	 */
 	public MusicPlayer(String fileName) {
+		//CSSE376 LAB
+		// Try to obtain existing clip first.
+		if(clips.containsKey(fileName)){
+			this.clip = clips.get(fileName);
+			return;
+		}
+		
 		// FIXME: reduce the number of calls to the code below
 		// Obtain a clip.
 		try {
@@ -39,6 +49,7 @@ public class MusicPlayer {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		clips.put(fileName, clip);
 	}
 	
 	/**
